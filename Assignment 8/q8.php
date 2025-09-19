91d16aa901076bf435740fff57ce2a80a92ee026
Sagar Awasthi <sagarawasthi852@gmail.com>
2025-09-19 15:25:13 +0530

    Create q8.php

---
+++ Assignment 8/q8.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+    $a=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
+    sort($a);
+    print_r($a);
+    rsort($a);
+    print_r($a);
+    ksort($a);
+    print_r($a);
+    krsort($a);
+    print_r($a);
+?>
+
+</body>
+</html>
+
+
